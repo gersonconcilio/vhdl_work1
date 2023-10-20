d2a615c9cf436474da985f7f57d014a304f18c5d
Gérson Concílio <gdmunhos@gmail.com>
2023-10-20 13:45:16 -0300

    Add files via upload

--- main.vhd
+++ main.vhd
@@ -23,18 +23,38 @@ signal pc  : std_logic_vector((DATA_WIDTH - 1) downto 0);
 signal acc : std_logic_vector((DATA_WIDTH - 1) downto 0);
 signal rdm : std_logic_vector((ADDR_WIDTH - 1) downto 0);
 
--- --------- SINAIS QUE NÃO TENHO CERTEZA --------------------------------
+-- --------- SINAIS QUE NÃO TENHO CERTEZA ---------------------------------
 signal sel : std_logic_vector((DATA_WIDTH - 2) downto 0); -- seletor do mux
 signal mul : std_logic_vector((ADDR_WIDTH - 1) downto 0); -- suporte multip
--- -----------------------------------------------------------------------
+-- ------------------------------------------------------------------------
 
 signal z   : std_logic;
 signal n   : std_logic;
 
+-- ----------------------------MEMORIA-------------------------------------
+type mem_dec is array (integer range 0 to 15) of std_logic_vector((ADDR_WIDTH - 1) downto 0);
+signal mem  : mem_dec;
+signal addr : std_logic_vector((ADDR_WIDTH - 1) downto 0);
 
 begin
 
-    mul <= acc * rdm(3 downto 0);
+    mul <= acc * rdm(3 downto 0); -- MULTIPLICACAO FORA DO PROCESS
+
+    mem(0) <= "00000000";
+    mem(1) <= "00000000";
+    mem(2) <= "00000000";
+    mem(3) <= "00000000";
+    mem(4) <= "00000000";
+    mem(5) <= "00000000";
+    mem(6) <= "00000000";
+    mem(7) <= "00000000";
+    mem(8) <= "00000000";
+    mem(9) <= "00000000";
+    mem(10) <= "00000000";
+    mem(11) <= "00000000";
+    mem(12) <= "00000000";
+    mem(13) <= "00000000";
+    mem(14) <= "00000000";
 
     process(clk, res)
         begin
@@ -47,7 +67,8 @@ begin
                 z   <= '0';
                 n   <= '0';
             elsif(rising_edge(clk))then
-
+                
+                rdm <= mem(pc); --POSSÍVEL FORMA DE COMO O SELECIONAR NA MEMORIA
                 sel <= rdm(5 downto 4); -- seletor do mux
                 
                 -- -------------O QUE SERIA O PC---------------------
