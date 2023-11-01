0492fc68119956a63526e89cce767d91ae46e58e
Gérson Concílio <gdmunhos@gmail.com>
2023-11-01 14:34:57 -0300

    tentativa de controle

--- controle.vhd
+++ controle.vhd
@@ -9,18 +9,93 @@ entity controle is
         DATA_WIDTH : natural := 4
     );
     port(
-        cont_load : in std_logic;
-        en_ula    : in std_logic;
+        clock     : in std_logic;
+--      reset     : in std_logic;
+        
+        op        : in std_logic_vector((DATA_WIDTH - 1) downto 0);
 
-        sai_op    : in std_logic
+        sai_op    : out std_logic_vector(2 downto 0)
     );
 end entity controle;
 
 architecture behavior of controle is 
 
-type t_contro_logic_fsm is (state_load,
+type control_logic_fsm is (state_load,
                             state_ula,
-                            state_jm);
+                            state_jmp);
+
+signal atual   : control_logic_fsm;
+signal proximo : control_logic_fsm;
 
 begin
+
+    fluxo : process(clock)
+    begin
+
+        if(rising_edge(clock))then
+            atual <= proximo;
+        end if;
+
+    end process fluxo;
+
+    -- --------------------------- AQUI ESTA O PRÓXIMO PASSO -----------------------------
+    --         AQUI ESTÁ FALTANDO O 'CONTROLE' QUE INDICARÁ
+    --         SE O ESTADO IRÁ SEGUIR NO MESMO OU IRÁ PARA
+    --         UM PRÓXIMO ESTADO
+
+    apos : process(clock)
+    begin
+
+        case atual is
+            when state_load =>
+                if(op = 4D"1")then
+                    proximo <= state_ula;
+                elsif(op = 4D"2")then
+                    proximo <= state_jmp;
+                else
+                    proximo <= state_load;
+                end if;
+            when state_ula  =>
+                if(op = 4D"0")then
+                    proximo <= state_load;
+                elsif(op = 4D"2")then
+                    proximo <= state_jmp;
+                else
+                    proximo <= state_ula;
+                end if;
+            when others     =>  --JUMP
+                if(op = 4D"0")then
+                    proximo <= state_load;
+                elsif(op = 4D"1")then
+                    proximo <= state_ula;
+                else
+                    proximo <= state_jmp;
+                end if;
+        end case;
+
+    end process;
+
+    -- ----------------- AQUI ESTA A SAIDA QUE VAI PARA A OPERATIVA ----------------------
+    saida : process(clock) --RESET????? 
+    begin
+
+        if(rising_edge(clock))then
+            case atual is 
+                when state_load =>
+                    sai_op(0) <= '1';
+                    sai_op(1) <= '1';
+                    sai_op(2) <= '0';
+                when state_ula  =>
+                    sai_op(0) <= '1';
+                    sai_op(1) <= '1';
+                    sai_op(2) <= '1';
+                when others     =>  --state_jmp
+                    sai_op(0) <= '0';
+                    sai_op(1) <= '1';
+                    sai_op(2) <= '0';
+            end case;
+        end if;
+
+    end process saida;
+
 end architecture behavior;
