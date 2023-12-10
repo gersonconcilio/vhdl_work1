a6644a8487e9c214f04eccbe5f3b2b23e2abe235
Gérson Concílio <gdmunhos@gmail.com>
2023-12-09 23:32:10 -0300

    controle e operativa prontos

--- controle.vhd
+++ controle.vhd
@@ -11,32 +11,39 @@ entity controle is
     port(
         clock     : in std_logic;
         reset     : in std_logic;
+        z         : in std_logic;
+        n         : in std_logic;
         
         op        : in std_logic_vector((ADDR_WIDTH - 1) downto 0);
 
-        sai_op    : out std_logic_vector((ADDR_WIDTH - 3) downto 0)
+        sai_op    : out std_logic_vector((DATA_WIDTH - 3) downto 0)
     );
 end entity controle;
 
 architecture behavior of controle is 
 
-type control_logic_fsm is (state_load,
+type control_logic_fsm is (state_anda,
                             state_ula,
                             state_jmp);
 
 signal atual     : control_logic_fsm;
 signal proximo   : control_logic_fsm;
+
 signal sinal_ula : std_logic;
+signal sinal_jmp : std_logic;
+signal sinal_jzn : std_logic;
 
 begin
 
     sinal_ula <= (op(0) or op(1) or op(2) or op(3));
+    sinal_jmp <= op(4);
+    sinal_jzn <= (op(5) or op(6));
 
     process(clock, reset)
     begin
 
         if(reset = '1')then
-            atual <= state_load;
+            atual <= state_anda;
         elsif(rising_edge(clock))then
             atual <= proximo;
         end if;
@@ -48,35 +55,37 @@ begin
     -- MAS NÃO NECESSÁRIAMENTE O TORNA NEGATIVO
     -- COMO PROCEDER?
     
-    apos : process(clock)
+    apos : process(atual, sinal_ula, sinal_jmp)
     begin
 
         case atual is
-            when state_load =>
-                if(op = 8D"1")then
-                    proximo <= state_ula;
-                elsif(op = 8D"2")then
+            when state_anda =>
+                if(sinal_ula = '1')then
+                    proximo <= state_ula; 
+                elsif(((sinal_jzn ='1') and (z = '1' or n = '1')) or (sinal_jmp = '1'))then
                     proximo <= state_jmp;
                 else
-                    proximo <= state_load;
-                end if;
-            when state_ula  =>
-                if(op = 8D"0")then
-                    proximo <= state_load;
-                elsif(op = 8D"2")then
-                    proximo <= state_jmp;
-                else
-                    proximo <= state_ula;
-                end if;
-            when others     =>  --JUMP
-                if(op = 8D"0")then
-                    proximo <= state_load;
-                elsif(op = 8D"1")then
-                    proximo <= state_ula;
-                else
-                    proximo <= state_jmp;
-                end if;
+                    proximo <= state_anda;    
+                end if; 
 
+           when state_ula  =>
+                if(sinal_jmp = '1')then
+                    proximo <= state_jmp; 
+                elsif(((sinal_jzn ='1') and (z = '1' or n = '1')) or (sinal_jmp = '1'))then
+                    proximo <= state_jmp;
+                else
+                    proximo <= state_anda;    
+                end if;  
+
+            when others => --JMP
+                if(sinal_ula = '1')then
+                    proximo <= state_ula; 
+                elsif(((sinal_jzn ='1') and (z = '1' or n = '1')) or (sinal_jmp = '1'))then
+                    proximo <= state_jmp;
+                else
+                    proximo <= state_anda;    
+                end if; 
+                
         end case;
 
     end process;
@@ -86,7 +95,7 @@ begin
     begin
         
         case atual is 
-            when state_load =>
+            when state_anda =>
                 sai_op(0) <= '1'; -- count_load
                 sai_op(1) <= '0'; -- en_ula
             when state_ula  =>
